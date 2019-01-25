b266cbe7eff1017ad1606df354063b4aff3ce8c0
mattdesl <dave.des@gmail.com>
2019-01-25 11:06:19 +0000

    make CLI an API instead of running automatically on require

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const budo = require('budo');
 const defined = require('defined');
+const parseArgs = require('minimist');
 const rightNow = require('right-now');
 const prettyBytes = require('pretty-bytes');
 const prettyMs = require('pretty-ms');
@@ -28,64 +29,6 @@ const fromString = require('from2-string');
 const pluginGLSL = require('./plugins/plugin-glsl');
 // const transformInstaller = require('./plugins/transform-installer');
 
-const argv = require('minimist')(process.argv.slice(2), {
-  string: ['template'],
-  boolean: [
-    'hot',
-    'open',
-    'force',
-    'pushstate',
-    'install',
-    'quiet',
-    'build',
-    'version',
-    'inline'
-  ],
-  alias: {
-    version: 'v',
-    port: 'p',
-    pushstate: 'P',
-    build: 'b',
-    dir: 'd',
-    open: 'o',
-    install: 'I',
-    force: 'f',
-    template: 't',
-    new: 'n'
-  },
-  '--': true,
-  default: {
-    install: true,
-    template: 'default'
-  }
-});
-
-if (argv.version) {
-  console.log(require('../package.json').version);
-  process.exit(0);
-}
-
-const templateDirectory = 'templates';
-const sketchDirectory = 'sketches';
-const defaultDir = '.';
-
-const cwd = argv.cwd || process.cwd();
-
-let dir;
-if (argv.dir) {
-  dir = path.isAbsolute(argv.dir) ? argv.dir : path.resolve(cwd, argv.dir);
-} else {
-  dir = path.resolve(cwd, defaultDir);
-}
-
-const templateHtmlFile = path.resolve(__dirname, 'templates/index.html');
-let htmlFile;
-if (argv.html) {
-  htmlFile = path.isAbsolute(argv.html) ? path.resolve(argv.html) : path.resolve(cwd, argv.html);
-} else {
-  htmlFile = templateHtmlFile;
-}
-
 const bundleAsync = (bundler) => {
   return new Promise((resolve, reject) => {
     bundler.bundle((err, src) => {
@@ -95,183 +38,68 @@ const bundleAsync = (bundler) => {
   });
 };
 
-const prepare = async (logger) => {
-  // Write a new package, but first check for collision
-  const dirName = path.basename(cwd);
-  if (dirName === 'canvas-sketch') {
-    const pkg = await readPackage({ cwd }, true);
-    if (!pkg || !isCanvasSketchPackage(pkg)) {
-      throw new Error(`Your folder name is ${chalk.bold('canvas-sketch')} which may lead to conflicts when using this tool. Please choose another folder name and run the command again.`);
+const start = async (args, overrides = {}) => {
+  const argv = parseArgs(args, {
+    string: ['template'],
+    boolean: [
+      'hot',
+      'open',
+      'force',
+      'pushstate',
+      'install',
+      'quiet',
+      'build',
+      'version',
+      'inline'
+    ],
+    alias: {
+      version: 'v',
+      port: 'p',
+      pushstate: 'P',
+      build: 'b',
+      dir: 'd',
+      open: 'o',
+      install: 'I',
+      force: 'f',
+      template: 't',
+      new: 'n'
+    },
+    '--': true,
+    default: {
+      install: true,
+      template: 'default'
     }
-  }
-
-  if (argv._.length > 1) {
-    throw new Error('Currently only one entry is supported.\n\nExample usage:\n    canvas-sketch src/index.js');
-  }
-
-  let entry = argv._[0];
-  delete argv._;
-  const browserifyArgs = argv['--'] || [];
-  delete argv['--'];
-
-  let resolveEntry;
-  let entrySrc;
-  if (argv.new) {
-    const prefix = typeof argv.new === 'string' ? argv.new : undefined;
-    let filepath;
-    if (entry) {
-      filepath = path.isAbsolute(entry) ? path.resolve(entry) : path.resolve(cwd, entry);
-
-      // ensure a file extension is present. If not, automatically add .js
-      if (!path.extname(entry)) {
-        filepath = `${filepath}.js`;
-      }
-    } else {
-      filepath = path.resolve(cwd, sketchDirectory, generateFileName(prefix));
-    }
-
-    if (!argv.force && fs.existsSync(filepath)) {
-      throw new Error(`The file already exists: ${path.relative(cwd, filepath)} (use -f to overwrite)`);
-    }
-
-    // Ensure the folder path exists
-    const fileDir = path.dirname(filepath);
-    await mkdirp(fileDir);
-
-    // Get stdin for piping
-    const stdin = (await getStdin()).trim();
-
-    if (stdin && (!argv.template || argv.template === 'default')) {
-      // Allow the user to pass in piped code
-      entrySrc = stdin;
-    } else {
-      let templateFile;
-      if (/^[\\/.]/.test(argv.template)) {
-        templateFile = path.isAbsolute(argv.template)
-          ? argv.template
-          : path.resolve(cwd, argv.template);
-        if (!fs.existsSync(templateFile)) {
-          throw new Error(`Couldn't find a template at ${argv.template}`);
-        }
-      } else {
-        templateFile = path.resolve(__dirname, templateDirectory, `${argv.template}.js`);
-        if (!fs.existsSync(templateFile)) {
-          throw new Error(`Couldn't find a template by the key ${argv.template}`);
-        }
-      }
-
-      resolveEntry = templateFile;
-      try {
-        entrySrc = fs.readFileSync(templateFile, 'utf-8');
-      } catch (err) {
-        throw new Error(`Error while reading the template ${argv.template}`);
-      }
-    }
-
-    logger.log(`Writing file: ${chalk.bold(path.relative(cwd, filepath))}`);
-    fs.writeFileSync(filepath, entrySrc);
-    entry = filepath;
-  }
-
-  if (!resolveEntry) resolveEntry = entry;
-
-  if (!entry) {
-    logger.error('No entry file specified!', `Example usage:\n    canvas-sketch src/index.js\n    canvas-sketch --new --template=regl`);
-    process.exit(1);
-  }
-
-  // Read source code
-  if (!entrySrc) {
-    try {
-      const entryPath = /^[.\//]/.test(entry) ? entry : ('./' + entry);
-      entry = resolve.sync(entryPath, { basedir: cwd });
-    } catch (err) {
-      logger.error(`Cannot find file "${chalk.bold(entry)}"`);
-      logger.pad();
-      process.exit(1);
-    }
-
-    try {
-      entrySrc = fs.readFileSync(entry, 'utf-8');
-    } catch (err) {
-      logger.error(`Cannot read entry file "${chalk.bold(path.relative(cwd, entry))}"`, err);
-      logger.pad();
-      process.exit(1);
-    }
-  }
-
-  // Install dependencies from the template if needed
-  if (argv.install !== false) {
-    try {
-      await install(resolveEntry, { entrySrc, logger, cwd });
-    } catch (err) {
-      console.error(err.toString());
-    }
-  }
-
-  let output = typeof argv.output !== 'undefined' ? argv.output : true;
-  if (typeof output === 'string' && /^(true|false)$/.test(output)) {
-    // handle string argv parsing
-    output = output === 'true';
-  }
-  if (output == null || output === true) {
-    // Default to downloads
-    output = downloads();
-  } else if (output === '.') {
-    // Accept '.' as current dir
-    output = cwd;
-  }
-
-  const mode = defined(argv.mode, argv.build ? 'production' : 'development');
-  const hot = Boolean(argv.hot);
-  const params = Object.assign({}, argv, {
-    mode,
-    browserifyArgs,
-    extensions: pluginGLSL.extensions,
-    output,
-    logger,
-    hot,
-    entry,
-    cwd,
-    installer: new EventEmitter()
   });
 
-  browserifyArgs.push(
-    // Add in ESM support
-    '-p', (bundler, opts) => {
-      return esmify(bundler, Object.assign({}, opts, {
-        // Disable "module" field since it is brutally annoying :(
-        // Basically it changes the way CommonJS-authored code needs to
-        // be written, forcing authors to update their code paths to use:
-        //   require('blah').default
-        // The added benefit of tree-shaking ES Modules isn't even used here (no rollup/webpack)
-        // so we will just discard it altogether for a cleaner developer & user experience.
-        mainFields: [ 'browser', 'main' ],
-        // This is a bit frustrating, as well. Babel-ifying the entire node_modules
-        // tree is extremely slow, and only fixes a few problematic modules
-        // that have decided to publish with ESM, which isn't even standard yet!
-        // So, we will only support ESM in local code for canvas-sketch.
-        nodeModules: false
-      }));
-    },
-    '-g', pluginGLSL(params),
-    // Add in glslify and make it resolve to here
-    '-g', require.resolve('glslify'),
-    // A plugin that handles resolving some modules to this CLI tool
-    '-p', pluginResolve(params),
-    // Also add in some envify tools
-    '-p', pluginEnv(params)
-  );
+  // Merge in user options
+  Object.assign(argv, overrides);
 
-  // TODO: Figure out a nice way to install automatically
-  // if (argv.install !== false) {
-  //   browserifyArgs.push('-t', transformInstaller(params));
-  // }
+  if (argv.version) {
+    console.log(require('../package.json').version);
+    process.exit(0);
+  }
 
-  return params;
-};
+  const templateDirectory = 'templates';
+  const sketchDirectory = 'sketches';
+  const defaultDir = '.';
 
-const start = async () => {
+  const cwd = argv.cwd || process.cwd();
+
+  let dir;
+  if (argv.dir) {
+    dir = path.isAbsolute(argv.dir) ? argv.dir : path.resolve(cwd, argv.dir);
+  } else {
+    dir = path.resolve(cwd, defaultDir);
+  }
+
+  const templateHtmlFile = path.resolve(__dirname, 'templates/index.html');
+  let htmlFile;
+  if (argv.html) {
+    htmlFile = path.isAbsolute(argv.html) ? path.resolve(argv.html) : path.resolve(cwd, argv.html);
+  } else {
+    htmlFile = templateHtmlFile;
+  }
+
   const logger = createLogger(argv);
   let opt;
   try {
@@ -367,6 +195,8 @@ const start = async () => {
       logger.log(`Finished in ${chalk.magenta(prettyMs(ms))}`, { leadingSpace: false });
       logger.pad();
     }
+
+    return null;
   } else {
     // pad the previous logs if necessary
     logger.pad();
@@ -442,7 +272,7 @@ const start = async () => {
         hasError = false;
       });
 
-      app.on('bundle-error', (err) => {
+      app.on('bundle-error', () => {
         hasError = true;
       });
     };
@@ -482,11 +312,191 @@ const start = async () => {
 
         applyReload(app, wss);
       });
+
+    return app;
+  }
+
+  async function prepare (logger) {
+    // Write a new package, but first check for collision
+    const dirName = path.basename(cwd);
+    if (dirName === 'canvas-sketch') {
+      const pkg = await readPackage({ cwd }, true);
+      if (!pkg || !isCanvasSketchPackage(pkg)) {
+        throw new Error(`Your folder name is ${chalk.bold('canvas-sketch')} which may lead to conflicts when using this tool. Please choose another folder name and run the command again.`);
+      }
+    }
+
+    if (argv._.length > 1) {
+      throw new Error('Currently only one entry is supported.\n\nExample usage:\n    canvas-sketch src/index.js');
+    }
+
+    let entry = argv._[0];
+    delete argv._;
+    const browserifyArgs = argv['--'] || [];
+    delete argv['--'];
+
+    let resolveEntry;
+    let entrySrc;
+    if (argv.new) {
+      const prefix = typeof argv.new === 'string' ? argv.new : undefined;
+      let filepath;
+      if (entry) {
+        filepath = path.isAbsolute(entry) ? path.resolve(entry) : path.resolve(cwd, entry);
+
+        // ensure a file extension is present. If not, automatically add .js
+        if (!path.extname(entry)) {
+          filepath = `${filepath}.js`;
+        }
+      } else {
+        filepath = path.resolve(cwd, sketchDirectory, generateFileName(prefix));
+      }
+
+      if (!argv.force && fs.existsSync(filepath)) {
+        throw new Error(`The file already exists: ${path.relative(cwd, filepath)} (use -f to overwrite)`);
+      }
+
+      // Ensure the folder path exists
+      const fileDir = path.dirname(filepath);
+      await mkdirp(fileDir);
+
+      // Get stdin for piping
+      const stdin = (await getStdin()).trim();
+
+      if (stdin && (!argv.template || argv.template === 'default')) {
+        // Allow the user to pass in piped code
+        entrySrc = stdin;
+      } else {
+        let templateFile;
+        if (/^[\\/.]/.test(argv.template)) {
+          templateFile = path.isAbsolute(argv.template)
+            ? argv.template
+            : path.resolve(cwd, argv.template);
+          if (!fs.existsSync(templateFile)) {
+            throw new Error(`Couldn't find a template at ${argv.template}`);
+          }
+        } else {
+          templateFile = path.resolve(__dirname, templateDirectory, `${argv.template}.js`);
+          if (!fs.existsSync(templateFile)) {
+            throw new Error(`Couldn't find a template by the key ${argv.template}`);
+          }
+        }
+
+        resolveEntry = templateFile;
+        try {
+          entrySrc = fs.readFileSync(templateFile, 'utf-8');
+        } catch (err) {
+          throw new Error(`Error while reading the template ${argv.template}`);
+        }
+      }
+
+      logger.log(`Writing file: ${chalk.bold(path.relative(cwd, filepath))}`);
+      fs.writeFileSync(filepath, entrySrc);
+      entry = filepath;
+    }
+
+    if (!resolveEntry) resolveEntry = entry;
+
+    if (!entry) {
+      logger.error('No entry file specified!', `Example usage:\n    canvas-sketch src/index.js\n    canvas-sketch --new --template=regl`);
+      process.exit(1);
+    }
+
+    // Read source code
+    if (!entrySrc) {
+      try {
+        const entryPath = /^[.\//]/.test(entry) ? entry : ('./' + entry);
+        entry = resolve.sync(entryPath, { basedir: cwd });
+      } catch (err) {
+        logger.error(`Cannot find file "${chalk.bold(entry)}"`);
+        logger.pad();
+        process.exit(1);
+      }
+
+      try {
+        entrySrc = fs.readFileSync(entry, 'utf-8');
+      } catch (err) {
+        logger.error(`Cannot read entry file "${chalk.bold(path.relative(cwd, entry))}"`, err);
+        logger.pad();
+        process.exit(1);
+      }
+    }
+
+    // Install dependencies from the template if needed
+    if (argv.install !== false) {
+      try {
+        await install(resolveEntry, { entrySrc, logger, cwd });
+      } catch (err) {
+        console.error(err.toString());
+      }
+    }
+
+    let output = typeof argv.output !== 'undefined' ? argv.output : true;
+    if (typeof output === 'string' && /^(true|false)$/.test(output)) {
+      // handle string argv parsing
+      output = output === 'true';
+    }
+    if (output == null || output === true) {
+      // Default to downloads
+      output = downloads();
+    } else if (output === '.') {
+      // Accept '.' as current dir
+      output = cwd;
+    }
+
+    const mode = defined(argv.mode, argv.build ? 'production' : 'development');
+    const hot = Boolean(argv.hot);
+    const params = Object.assign({}, argv, {
+      mode,
+      browserifyArgs,
+      extensions: pluginGLSL.extensions,
+      output,
+      logger,
+      hot,
+      entry,
+      cwd,
+      installer: new EventEmitter()
+    });
+
+    browserifyArgs.push(
+      // Add in ESM support
+      '-p', (bundler, opts) => {
+        return esmify(bundler, Object.assign({}, opts, {
+          // Disable "module" field since it is brutally annoying :(
+          // Basically it changes the way CommonJS-authored code needs to
+          // be written, forcing authors to update their code paths to use:
+          //   require('blah').default
+          // The added benefit of tree-shaking ES Modules isn't even used here (no rollup/webpack)
+          // so we will just discard it altogether for a cleaner developer & user experience.
+          mainFields: [ 'browser', 'main' ],
+          // This is a bit frustrating, as well. Babel-ifying the entire node_modules
+          // tree is extremely slow, and only fixes a few problematic modules
+          // that have decided to publish with ESM, which isn't even standard yet!
+          // So, we will only support ESM in local code for canvas-sketch.
+          nodeModules: false
+        }));
+      },
+      '-g', pluginGLSL(params),
+      // Add in glslify and make it resolve to here
+      '-g', require.resolve('glslify'),
+      // A plugin that handles resolving some modules to this CLI tool
+      '-p', pluginResolve(params),
+      // Also add in some envify tools
+      '-p', pluginEnv(params)
+    );
+
+    // TODO: Figure out a nice way to install automatically
+    // if (argv.install !== false) {
+    //   browserifyArgs.push('-t', transformInstaller(params));
+    // }
+
+    return params;
   }
 };
 
-start()
-  .catch(err => {
+module.exports = start;
+
+if (!module.parent) {
+  module.exports(process.argv.slice(2)).catch(err => {
     const { message, stack } = getErrorDetails(err);
     if (err instanceof SyntaxError) {
       console.error(`\n${err.toString()}\n`);
@@ -500,3 +510,4 @@ start()
       ].join('\n'));
     }
   });
+}
