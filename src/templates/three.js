5bd58d954d6a47225738bc30b8c10da0315ee827
mattdesl <dave.des@gmail.com>
2019-10-24 16:02:32 +0100

    make three template a little simpler

--- src/templates/three.js
+++ src/templates/three.js
@@ -1,16 +1,16 @@
 // Ensure ThreeJS is in global scope for the 'examples/'
-global.THREE = require('three');
+global.THREE = require("three");
 
 // Include any additional ThreeJS examples below
-require('three/examples/js/controls/OrbitControls');
+require("three/examples/js/controls/OrbitControls");
 
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
-  context: 'webgl',
+  context: "webgl",
   // Turn on MSAA
   attributes: { antialias: true }
 };
@@ -18,15 +18,15 @@ const settings = {
 const sketch = ({ context }) => {
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
-    context
+    canvas: context.canvas
   });
 
   // WebGL background color
-  renderer.setClearColor('#000', 1);
+  renderer.setClearColor("#000", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
+  const camera = new THREE.PerspectiveCamera(50, 1, 0.01, 100);
+  camera.position.set(0, 0, -4);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -35,41 +35,35 @@ const sketch = ({ context }) => {
   // Setup your scene
   const scene = new THREE.Scene();
 
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshPhysicalMaterial({
-      color: 'white',
-      roughness: 0.75,
-      flatShading: true
-    })
-  );
+  // Setup a geometry
+  const geometry = new THREE.SphereGeometry(1, 32, 16);
+
+  // Setup a material
+  const material = new THREE.MeshBasicMaterial({
+    color: "red",
+    wireframe: true
+  });
+
+  // Setup a mesh with geometry + material
+  const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
-
-  // Specify an ambient/unlit colour
-  scene.add(new THREE.AmbientLight('#59314f'));
-
-  // Add some light
-  const light = new THREE.PointLight('#45caf7', 1, 15.5);
-  light.position.set(2, 2, -4).multiplyScalar(1.5);
-  scene.add(light);
 
   // draw each frame
   return {
     // Handle resize events here
-    resize ({ pixelRatio, viewportWidth, viewportHeight }) {
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight);
       camera.aspect = viewportWidth / viewportHeight;
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
-    render ({ time }) {
-      mesh.rotation.y = time * (10 * Math.PI / 180);
+    render({ time }) {
       controls.update();
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
-    unload () {
+    unload() {
       controls.dispose();
       renderer.dispose();
     }
