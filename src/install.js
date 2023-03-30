89d4ef8809505520f9b4db4def7f2fbbc709d82e
mattdesl <info@mattdesl.studio>
2023-03-30 17:12:29 +0900

    fix three template; improve installation log by removing noise and warnings

--- src/install.js
+++ src/install.js
@@ -3,6 +3,7 @@ const installIfNeeded = promisify(require('install-if-needed'));
 const { isCanvasSketchPackage, readPackage } = require('./util');
 const isBuiltin = require('is-builtin-module');
 const packageName = require('require-package-name');
+const semver = require('semver');
 const chalk = require('chalk');
 const { exec } = require('child_process');
 const path = require('path');
@@ -43,7 +44,7 @@ module.exports = async function (entry, opt = {}) {
     throw err;
   }
 
-  const dependencies = requires
+  let dependencies = requires
     .filter(req => !/^[./\\/]/.test(req) && !ignore.includes(req))
     .map(req => packageName(req))
     .filter(req => !isBuiltin(req))
@@ -61,6 +62,53 @@ module.exports = async function (entry, opt = {}) {
   const currentDeps = Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.devDependencies || {}));
   let filtered = dependencies.filter(dep => !currentDeps.includes(dep));
 
+  // patch to fix ThreeJS :'(
+  // we are locked on an earlier version of ThreeJS unless
+  // canvas-sketch is overhauled to esbuild or the esm loader is fixed
+  const threeIdx = dependencies.indexOf('three');
+  if (threeIdx >= 0) {
+    const fixedVers = '0.147.0';
+    const toInstall = `three@${fixedVers}`;
+
+    
+    const hasThreeDep = currentDeps.includes('three');
+    if (!hasThreeDep) {
+      // Case A: User does not have ThreeJS already in package.json
+      // So we can just add the versioned tag to installation
+      filtered = filtered.map(f => f === 'three' ? toInstall : f);
+
+      // filtered.push(toInstall)
+      console.warn(chalk.red(
+        `
+~~~~~~~ NOTE ~~~~~~~
+canvas-sketch currently only supports older versions
+of ThreeJS, the CLI will auto-install version ${fixedVers}.
+~~~~~~~~~~~~~~~~~~~~`
+      ));
+    } else {
+      // Case B: User does have it in package.json
+      // Let's compare versions to see if we should warn them or not
+      const deps = pkg.dependencies || {};
+      const devDeps = pkg.devDependencies || {};
+      const threeDep = 'three' in deps ? deps['three'] : devDeps['three'];
+      if (threeDep && !semver.intersects(`<=${fixedVers}`, threeDep)) {
+        console.warn(chalk.red(
+          `
+~~~~~~~ NOTE ~~~~~~~
+canvas-sketch currently only supports older versions
+of ThreeJS, and this package specifies a higher version number.
+You can re-install ${toInstall} to down-grade to a working version:
+
+  npm install ${toInstall} --save-dev
+
+~~~~~~~~~~~~~~~~~~~~`
+        ));
+      } else {
+        // A-OK - user is on the right version range
+      }
+    }
+  }
+
   let key = 'dependencies';
   const canvasSketchModule = 'canvas-sketch';
   if (isCanvasSketchPackage(pkg)) {
@@ -73,7 +121,7 @@ module.exports = async function (entry, opt = {}) {
 
   // Only install if needed
   if (filtered.length > 0) {
-    const obj = { stdio: 'inherit' };
+    const obj = { stdio: 'inherit', audit: false, fund: false, silent: true };
     obj[key] = filtered;
 
     if (logger) {
